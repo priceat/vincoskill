7b6a034fcc0a4fa655994c08ae01ab3b9b7f2e3b
Tate Price <priceat@gmail.com>
2015-05-17 16:11:20 -0500

    fixed mailer

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,10 +1,10 @@
-class NotificationMailer < ActionMailer::Base
-  default from: "admin@vincoskill.com"
+# class NotificationMailer < ActionMailer::Base
+#   default from: "admin@vincoskill.com"
 
-  def new_player(user)
-    @team = current_user.team
-    if @user.role == "player"
-    mail(to: ,
-      subject: "Welcome to VincoSkill")
-  end
-end
+#   def new_player(user)
+#     @team = current_user.team
+#     if @user.role == "player"
+#     mail(to: ,
+#       subject: "Welcome to VincoSkill")
+#   end
+# end
